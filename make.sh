fbf3956ef1ecb0f1b69626b9ce1f94c1e53ed8c3
exegoist <exegoist@icloud.com>
2013-12-09 22:30:07 +0200

    initial dotfiles

---
+++ make.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+dir=~/dotfiles
+originals=~/dotfiles/originals
+files=`ls $dir | grep -v originals | grep -v *.sh`
+
+echo "Creating directory $originals for backup of original dotfiles in ~"
+mkdir -p $originals
+echo "...done"
+
+echo "Changing to the $dir"
+cd $dir
+echo "...done"
+
+for file in $files; do
+	echo "Moving any existing dotfiles from ~ to $originals"
+	mv ~/.$file $originals/
+	echo "Creating symlink to $file in home directory"
+	ln -s $dir/$file ~/.$file
+done
---
+++ vimrc
@@ -0,0 +1,14 @@
+set nocompatible              " be iMproved
+filetype off                  " required!
+
+set rtp+=~/.vim/bundle/vundle/
+call vundle#rc()
+
+" let Vundle manage Vundle
+" " required! 
+Bundle 'gmarik/vundle'
+filetype plugin indent on     " required!
+Bundle "MarcWeber/vim-addon-mw-utils"
+Bundle "tomtom/tlib_vim"
+Bundle "garbas/vim-snipmate"
+Bundle "honza/vim-snippets"
