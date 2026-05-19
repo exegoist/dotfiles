fe0747c020c8aed449a5e4440e75e11f4388f17c
exegoist <exegoist@icloud.com>
2026-05-19 21:29:52 +0300

    update neovim config according to 0.12

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,17 +1,7 @@
 -- Install 'mini.nvim' with 'mini.deps'
-local path_package = vim.fn.stdpath('data') .. '/site/'
-local mini_path = path_package .. 'pack/deps/start/mini.nvim'
-if not vim.loop.fs_stat(mini_path) then
-  vim.cmd('echo "Installing `mini.nvim`" | redraw')
-  local clone_cmd = {
-    'git', 'clone', '--filter=blob:none',
-    '--branch', 'stable',
-    'https://github.com/echasnovski/mini.nvim', mini_path
-  }
-  vim.fn.system(clone_cmd)
-  vim.cmd('packadd mini.nvim | helptags ALL')
-  vim.cmd('echo "Installed `mini.nvim`" | redraw')
-end
+vim.pack.add({
+  { src = 'https://github.com/nvim-mini/mini.nvim', version = 'stable' },
+})
 
 -- Set up 'mini.deps'
 require('mini.deps').setup({ path = { package = path_package } })
